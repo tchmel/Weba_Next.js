83402604babc11d6690e1eeb35cb6dcc50ccf816
Tobias <tobias.chmel@gmx.at>
2022-05-29 15:11:08 +0200

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ function MyApp({ Component, pageProps }) {
 
         <img
           id="title"
-          src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Pet_logo_with_flowers.png"
+          src="https://www.pngkey.com/png/full/697-6975794_pet-logo-png.png"
           alt="pet care logo"
         ></img>
       </div>
